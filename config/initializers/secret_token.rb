a2d85fa170f39a46b660dab9b2bd9d4515d61c0c
Julian McMorris <jmcmorris@gmail.com>
2013-10-10 22:04:08 -0500

    Initil Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirsApp::Application.config.secret_key_base = '6d52e7bf828ffe7614ad677fce63afb98757ecc29cd4b7d957e6b8c2f97721e9da8f55bbdaf6a486cc642f3a99128878cac6762ab6361c104af684b6906ad56e'
